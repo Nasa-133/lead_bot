d1b523a3e4a3e45ea0b9c43612b768fa96665946
Nasa-133 <your@email.com>
2025-10-19 12:03:02 +0500

    remove_comment

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 # .env faylini yuklash
 load_dotenv()
 
-# Bot tokeningiz va kanal ID .env faylidan olinadi
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
@@ -14,20 +13,15 @@ CHANNEL_ID = os.getenv("CHANNEL_ID")
 async def forward_to_channel(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Kelgan har qanday xabarni kanalga yuborish"""
     try:
-        # Xabarni kanalga forward qilish (eng oddiy usul)
+
         await update.message.forward(chat_id=CHANNEL_ID)
 
-        # Yoki o'zingiz formatlasangiz:
-        # await context.bot.send_message(
-        #     chat_id=CHANNEL_ID,
-        #     text=update.message.text
-        # )
 
     except Exception as e:
         print(f"Xatolik: {e}")
 
 
-# Botni ishga tushirish
+
 app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(MessageHandler(filters.ALL, forward_to_channel))
 
